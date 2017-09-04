9d87b63eca9b85a3b2f1dea1bb6a45ea6c01013f
Alex Ha <alexh.90@live.com>
2017-09-04 15:58:42 -0700

    Add validation

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import _ from 'lodash';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPosts } from "../actions/index"
 
 class PostsIndex extends React.Component {
@@ -22,6 +23,11 @@ class PostsIndex extends React.Component {
     render() {
         return (
           <div>
+              <div className="text-xs-right">
+                  <Link className="btn btn-primary" to="/posts/new">
+                      Add a post
+                  </Link>
+              </div>
               <h3>Posts</h3>
               <ul className="list-group">
                   {this.renderPosts()}
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,15 +1,80 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends React.Component {
-    render() {
+    renderField(field) {
         return (
-            <div>
-                new posts!!
+          <div className="form-group">
+              <label>{field.label}</label>
+              <input
+                  className="form-control"
+                  type="text"
+                  {...field.input}
+              />
+              {field.meta.error}
+          </div>
+        );
+    }
 
-            </div>
+    onSubmit(values) {
+        console.log(values);
+    }
+
+
+    render() {
+        const { handleSubmit } = this.props;
+
+        return (
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+                <Field
+                    label="Post Title"
+                    name="title"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Categories"
+                    name="categories"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Post Content"
+                    name="content"
+                    component={this.renderField}
+                />
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
         );
     }
 }
 
 
-export default PostsNew;
+function validate(values) {
+    const errors = {};
+
+    /*
+    if (values.title.length < 3) {
+        errors.title = "Title must be at least three characters long!";
+    }
+    */
+
+    if (!values.title) {
+        errors.title = "Please enter a title!";
+    }
+
+    if (!values.categories) {
+        errors.categories = "Please enter some categories";
+    }
+
+    if (!values.content) {
+        errors.content = "Please enter some content";
+    }
+
+    return errors;
+}
+
+
+
+export default reduxForm({
+    validate: validate,
+    form: 'PostsNewForm'
+})(PostsNew);
